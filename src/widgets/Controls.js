170779fd52e6593272457a82704df420a50d8ae6
Rodrigo Alfonso <rodri042@gmail.com>
2019-07-07 21:55:04 -0300

    Now Controls shows the real controls

--- src/widgets/Controls.js
+++ src/widgets/Controls.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import config from "../config";
 import strings from "../locales";
 import styles from "./Controls.module.css";
 import classNames from "classnames";
+import _ from "lodash";
+
+const FRIENDLY_NAMES = {
+	ArrowLeft: "🠜",
+	ArrowRight: "🠞",
+	ArrowUp: "🠝",
+	ArrowDown: "🠟",
+	" ": "[Space]"
+};
 
 export default class Controls extends Component {
 	render() {
@@ -12,11 +22,29 @@ export default class Controls extends Component {
 				</b>
 				<br />
 				<div className={styles.controller}>
-					<span className={styles.dpad}>🠜 🠞 🠝 🠟</span>
-					<span className={styles.buttons}>d [space]</span>
+					<span className={styles.dpad}>
+						{this._keyFor("BUTTON_LEFT")} {this._keyFor("BUTTON_RIGHT")}{" "}
+						{this._keyFor("BUTTON_UP")} {this._keyFor("BUTTON_DOWN")}
+					</span>
+					<span className={styles.buttons}>
+						{this._keyFor("BUTTON_B")} {this._keyFor("BUTTON_A")}
+					</span>
 				</div>
-				<div className={styles.centered}>[supr] [enter]</div>
+				<div className={styles.centered}>
+					{this._keyFor("BUTTON_SELECT")} {this._keyFor("BUTTON_START")}
+				</div>
 			</div>
+		);
+	}
+
+	_keyFor(button) {
+		const key = _.findKey(config.options.input, (it) => it === button);
+		return this._format(key);
+	}
+
+	_format(key) {
+		return (
+			FRIENDLY_NAMES[key] || (key.length === 1 ? key.toUpperCase() : `[${key}]`)
 		);
 	}
 }
